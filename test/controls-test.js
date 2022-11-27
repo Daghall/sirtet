ef04ec2543a3d973ce885f8fb1daa6745760d59c
Markus Daghall <markus@daghall.se>
2022-11-27 15:16:13 +0100

    Move square shape left/right

--- test/controls-test.js
+++ test/controls-test.js
@@ -7,11 +7,11 @@ import Shape from "../lib/shape.js";
 Feature("controls", () => {
   Scenario("move square shape up", () => {
     let board;
-    let controls;
     Given("a board and a square one block from the top", () => {
       board = new Board(new Shape(Shape.SHAPES.SQUARE), 1, 1);
     });
 
+    let controls;
     When("controls are initialized", () => {
       controls = new Controls(board);
     });
@@ -39,11 +39,11 @@ Feature("controls", () => {
 
   Scenario("move square shape down", () => {
     let board;
-    let controls;
     Given("a board and a square one block from the bottom", () => {
       board = new Board(new Shape(Shape.SHAPES.SQUARE), 1, Board.MAX_Y - 2);
     });
 
+    let controls;
     When("controls are initialized", () => {
       controls = new Controls(board);
     });
@@ -66,6 +66,70 @@ Feature("controls", () => {
 
     Then("square should be at the same place", () => {
       expect(board.current.toString()).to.deep.equal("1:18,1:19,2:18,2:19");
+    });
+  });
+
+  Scenario("move square shape left", () => {
+    let board;
+    Given("a board and a square one block from the left wall", () => {
+      board = new Board(new Shape(Shape.SHAPES.SQUARE), 1, 1);
+    });
+
+    let controls;
+    When("controls are initialized", () => {
+      controls = new Controls(board);
+    });
+
+    Then("square should be near the top", () => {
+      expect(board.current.toString()).to.deep.equal("1:1,1:2,2:1,2:2");
+    });
+
+    When("moving square left", () => {
+      controls.left();
+    });
+
+    Then("square should be moved", () => {
+      expect(board.current.toString()).to.deep.equal("0:1,0:2,1:1,1:2");
+    });
+
+    When("trying to move beyond left wall", () => {
+      controls.left();
+    });
+
+    Then("square should be at the same place", () => {
+      expect(board.current.toString()).to.deep.equal("0:1,0:2,1:1,1:2");
+    });
+  });
+
+  Scenario("move square shape right", () => {
+    let board;
+    Given("a board and a square one block from the right wall", () => {
+      board = new Board(new Shape(Shape.SHAPES.SQUARE), Board.MAX_X - 2, 1);
+    });
+
+    let controls;
+    When("controls are initialized", () => {
+      controls = new Controls(board);
+    });
+
+    Then("square should be near the top", () => {
+      expect(board.current.toString()).to.deep.equal("7:1,7:2,8:1,8:2");
+    });
+
+    When("moving square right", () => {
+      controls.right();
+    });
+
+    Then("square should be moved", () => {
+      expect(board.current.toString()).to.deep.equal("8:1,8:2,9:1,9:2");
+    });
+
+    When("trying to move beyond right wall", () => {
+      controls.right();
+    });
+
+    Then("square should be at the same place", () => {
+      expect(board.current.toString()).to.deep.equal("8:1,8:2,9:1,9:2");
     });
   });
 });
